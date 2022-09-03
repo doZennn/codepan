163942298893f995d22abab400749199ca60f237
Jozen Blue Martinez <me@jozen.blue>
2022-09-03 17:59:43 +0800

    Remove Twitter and version

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -162,18 +162,6 @@
           <el-dropdown-item style="padding: 0;">
             <a class="el-dropdown-menu__item fake-anchor" target="_blank" href="https://github.com/egoist/codepan"><link2-icon></link2-icon> Source Code</a>
           </el-dropdown-item>
-          <el-dropdown-item style="padding: 0;">
-            <a class="el-dropdown-menu__item fake-anchor" target="_blank" href="https://twitter.com/_egoistlily"><twitter-icon></twitter-icon> Follow me on Twitter</a>
-          </el-dropdown-item>
-          <el-dropdown-item style="padding: 0;">
-            <a
-              target="_blank"
-              class="el-dropdown-menu__item fake-anchor"
-              :href="`https://github.com/egoist/codepan/commit/${latestCommit}`">
-              <info-icon></info-icon>
-              {{ version }}
-            </a>
-          </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
       <a
